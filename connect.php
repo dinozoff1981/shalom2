f54469f34302a44628f8b57b83bfa5ae7a9957b9
dinozoff1981 <tensaeg@yahoo.com>
2022-10-08 21:00:33 +0200

    new db

--- connect.php
+++ connect.php
@@ -5,14 +5,15 @@ $con=new mysqli("remotemysql.com","eaz1Ivi4PE","pgTGK9YvMB","eaz1Ivi4PE");
 
 
 if(!$con){
-    die('Connection Failed'. mysqli_connect_error($con));
+    //die('Connection Failed'. mysqli_connect_error($con));
+
 }
 
 
 //$con=new mysqli("localhost","root","","shalomdb");
 
 //if(!$con){
-   //die('Connection Failed'. mysqli_connect_error($con));
+  // die('Connection Failed'. mysqli_connect_error($con));
 //}
 
 ?>
