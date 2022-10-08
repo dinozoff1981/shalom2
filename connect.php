bfc8d40d1cf0b1c9b13d5ed7a4c87c39041a1a42
dinozoff1981 <tensaeg@yahoo.com>
2022-10-08 20:36:39 +0200

    newconn

--- connect.php
+++ connect.php
@@ -1,18 +1,18 @@
 <?php
 
 
-//$con=new mysqli("remotemysql.com","eaz1Ivi4PE","pgTGK9YvMB","eaz1Ivi4PE");
+$con=new mysqli("remotemysql.com","eaz1Ivi4PE","pgTGK9YvMB","eaz1Ivi4PE");
 
-
-//if(!$con){
-   // die('Connection Failed'. mysqli_connect_error($con));
-//}
-
-
-$con=new mysqli("localhost","root","","shalomdb");
 
 if(!$con){
-   die('Connection Failed'. mysqli_connect_error($con));
+    die('Connection Failed'. mysqli_connect_error($con));
 }
+
+
+//$con=new mysqli("localhost","root","","shalomdb");
+
+//if(!$con){
+   //die('Connection Failed'. mysqli_connect_error($con));
+//}
 
 ?>
